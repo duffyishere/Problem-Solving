47f432503ade53e4255b6ed338fb10e73d4cbc59
zzunho9 <jamlove4763@naver.com>
2023-04-06 18:42:25 +0900

    feat: Add 1929 problem

--- java/src/main/java/org/duffy/Main.java
+++ java/src/main/java/org/duffy/Main.java
@@ -2,7 +2,6 @@ package org.duffy;
 
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
--- java/src/main/java/org/duffy/math/MathProblem.java
+++ java/src/main/java/org/duffy/math/MathProblem.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
 public class MathProblem {
@@ -32,5 +33,26 @@ public class MathProblem {
                 return false;
         }
         return true;
+    }
+
+    public void pr1929() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.valueOf(st.nextToken());
+        int n = Integer.valueOf(st.nextToken());
+        boolean[] check = new boolean[n+1];
+        check[0] = check[1] = true;
+        for (int i=2; i*i<=n; i++) {
+            if (!check[i]) {
+                for (int j=i*i; j<=n; j+=i) {
+                    check[j] = true;
+                }
+            }
+        }
+        for (int i=m; i<=n; i++) {
+            if (!check[i]) {
+                System.out.println(i);
+            }
+        }
     }
 }
