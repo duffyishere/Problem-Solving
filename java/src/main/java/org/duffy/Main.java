d143847b76ca2a9abc3610e57f26df4c4fc2dc78
zzunho9 <jamlove4763@naver.com>
2023-04-14 16:40:00 +0900

    feat: Add 10844 problem

--- java/src/main/java/org/duffy/Main.java
+++ java/src/main/java/org/duffy/Main.java
@@ -3,11 +3,12 @@ package org.duffy;
 import org.duffy.dynamicProgramming.DynamicProgramming;
 
 import java.io.*;
-import java.util.*;
 
 public class Main {
 
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.valueOf(br.readLine());
     }
 }
--- java/src/main/java/org/duffy/dynamicProgramming/DynamicProgramming.java
+++ java/src/main/java/org/duffy/dynamicProgramming/DynamicProgramming.java
@@ -120,4 +120,30 @@ public class DynamicProgramming {
             System.out.println((dd[t][1] + dd[t][2] + dd[t][3])%mod);
         }
     }
+
+    public void pr10844() throws IOException {
+        long mod = 1000000000L;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.valueOf(br.readLine());
+        int[][] d = new int[n+1][10];
+        for (int i=1; i<10; i++)
+            d[1][i] = 1;
+
+        for (int i=2; i<=n; i++) {
+            for (int j=0; j<10; j++) {
+                d[i][j] = 0;
+                if (j-1 >= 0)
+                    d[i][j] += d[i-1][j-1];
+                if (j+1 <= 9)
+                    d[i][j] += d[i-1][j+1];
+                d[i][j] %= mod;
+            }
+        }
+
+        long result = 0;
+        for (int i=-0; i<10; i++)
+            result += d[n][i];
+
+        System.out.println(result%mod);
+    }
 }
