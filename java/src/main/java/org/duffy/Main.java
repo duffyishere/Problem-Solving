487cfca18d2b97bbef1002914b0bbb1774dc930f
zzunho9 <jamlove4763@naver.com>
2023-04-14 17:01:13 +0900

    feat: Add 2193 problem

--- java/src/main/java/org/duffy/Main.java
+++ java/src/main/java/org/duffy/Main.java
@@ -6,7 +6,6 @@ import java.io.*;
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.valueOf(br.readLine());
--- java/src/main/java/org/duffy/dynamicProgramming/DynamicProgramming.java
+++ java/src/main/java/org/duffy/dynamicProgramming/DynamicProgramming.java
@@ -146,4 +146,18 @@ public class DynamicProgramming {
 
         System.out.println(result%mod);
     }
+
+    public void pr2193() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.valueOf(br.readLine());
+        long[][] d = new long[n+2][2];
+        d[1][1] = d[2][0] = 1;
+
+        for (int i=3; i<=n; i++) {
+            d[i][0] = d[i-1][0] + d[i-1][1];
+            d[i][1] = d[i-1][0];
+        }
+
+        System.out.println(d[n][0] + d[n][1]);
+    }
 }
