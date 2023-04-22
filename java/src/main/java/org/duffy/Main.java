6a35c8e4a03ad4e0b4ef7f55eb3609a96a28e522
zzunho9 <jamlove4763@naver.com>
2023-04-22 16:48:07 +0900

    feat: Solve no.1309 problem

--- java/src/main/java/org/duffy/Main.java
+++ java/src/main/java/org/duffy/Main.java
@@ -5,6 +5,6 @@ import java.io.*;
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int n = Integer.valueOf(br.readLine());
     }
 }
