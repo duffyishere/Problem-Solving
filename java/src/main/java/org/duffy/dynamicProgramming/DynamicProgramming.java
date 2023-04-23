a70013f58a8035f64aec6c7dd52e4392e8a7d698
zzunho9 <jamlove4763@naver.com>
2023-04-23 17:42:49 +0900

    feat: Solve no.9465 problem

--- java/src/main/java/org/duffy/dynamicProgramming/DynamicProgramming.java
+++ java/src/main/java/org/duffy/dynamicProgramming/DynamicProgramming.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class DynamicProgramming {
 
@@ -329,5 +330,30 @@ public class DynamicProgramming {
         }
 
         System.out.println(d[k][n]);
+    }
+
+    public void pr9465() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] a = new int[2][n+1];
+            for (int i=0; i<2; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j=1; j<=n; j++) {
+                    a[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int[][] d = new int[n+1][3];
+            for (int i=1; i<=n; i++) {
+                d[i][0] = Math.max(d[i-1][0],Math.max(d[i-1][1],d[i-1][2]));
+                d[i][1] = Math.max(d[i-1][0],d[i-1][2])+a[0][i];
+                d[i][2] = Math.max(d[i-1][0],d[i-1][1])+a[1][i];
+            }
+            long ans = 0;
+            ans = Math.max(d[n][0], Math.max(d[n][1], d[n][2]));
+            System.out.println(ans);
+        }
     }
 }
