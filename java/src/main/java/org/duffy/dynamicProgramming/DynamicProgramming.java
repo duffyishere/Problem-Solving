deb10278bcd26bdb2276143cc5988e3989348c9b
zzunho9 <jamlove4763@naver.com>
2023-04-29 15:18:37 +0900

    feat: Resolve no.2193 problem

--- java/src/main/java/org/duffy/dynamicProgramming/DynamicProgramming.java
+++ java/src/main/java/org/duffy/dynamicProgramming/DynamicProgramming.java
@@ -164,6 +164,19 @@ public class DynamicProgramming {
         System.out.println(d[n][0] + d[n][1]);
     }
 
+    public void pr2193Re() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[][] d = new long[n+1][2];
+        d[1][1] = 1;
+        for (int i=2; i<=n; i++) {
+            d[i][0] = d[i-1][1] + d[i-1][0];
+            d[i][1] = d[i-1][0];
+        }
+
+        System.out.println(d[n][0] + d[n][1]);
+    }
+
     public void pr11053() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.valueOf(br.readLine());
