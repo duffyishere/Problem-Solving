4dda375105e90cf069d4263d1bb1f46c2a7cd4b8
zzunho9 <jamlove4763@naver.com>
2023-05-01 17:27:44 +0900

    feat: Resolve problem no.2133

--- java/src/main/java/org/duffy/Main.java
+++ java/src/main/java/org/duffy/Main.java
@@ -1,7 +1,6 @@
 package org.duffy;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String args[]) throws IOException {
--- java/src/main/java/org/duffy/dynamicProgramming/Practice.java
+++ java/src/main/java/org/duffy/dynamicProgramming/Practice.java
@@ -195,4 +195,19 @@ public class Practice {
         }
         System.out.println(ret);
     }
+
+    public void pr2133() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] d = new int[n+1];
+        d[0] = 1;
+        for (int i=2; i<=n; i++) {
+            d[i] = d[i-2] * 3;
+            for (int j=i-4; j>=0; j-=2) {
+                d[i] += d[j] * 2;
+            }
+        }
+
+        System.out.println(d[n]);
+    }
 }
