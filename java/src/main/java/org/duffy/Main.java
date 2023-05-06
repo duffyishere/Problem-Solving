27cd3c473c88cb7cb65fc0965d7d90f61a6b11e0
zzunho9 <jamlove4763@naver.com>
2023-05-06 23:37:44 +0900

    feat: Resolve problem no.3085

--- java/src/main/java/org/duffy/Main.java
+++ java/src/main/java/org/duffy/Main.java
@@ -7,4 +7,5 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
     }
+
 }
--- java/src/main/java/org/duffy/bruteForce/BruteForce.java
+++ java/src/main/java/org/duffy/bruteForce/BruteForce.java
@@ -29,4 +29,65 @@ public class BruteForce {
             }
         }
     }
+
+    public void pr3085() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        char[][] a = new char[n][n];
+        for (int i=0; i<n; i++) {
+            a[i] = br.readLine().toCharArray();
+        }
+
+        int ret = 0;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if (j+1 < n ) {
+                    char tmp = a[i][j];
+                    a[i][j] = a[i][j+1];
+                    a[i][j+1] = tmp;
+
+                    ret = Math.max(ret, check3085(a));
+
+                    tmp = a[i][j];
+                    a[i][j] = a[i][j+1];
+                    a[i][j+1] = tmp;
+                }
+                if (i+1 < n) {
+                    char tmp = a[i][j];
+                    a[i][j] = a[i+1][j];
+                    a[i+1][j] = tmp;
+
+                    ret = Math.max(ret, check3085(a));
+
+                    tmp = a[i][j];
+                    a[i][j] = a[i+1][j];
+                    a[i+1][j] = tmp;
+                }
+            }
+        }
+        System.out.println(ret);
+    }
+
+    private int check3085(char[][] a) {
+        int ret = 1;
+        int l = a.length;
+        for (int i=0; i<l; i++) {
+            // 가로 비교
+            int tmp = 1;
+            for (int j=1; j<l; j++) {
+                if (a[i][j] == a[i][j-1]) tmp++;
+                else tmp = 1;
+                ret = Math.max(ret, tmp);
+            }
+            // 세로 비교
+            tmp = 1;
+            for (int j=1; j<l; j++) {
+                if (a[j][i] == a[j-1][i]) tmp++;
+                else tmp = 1;
+                ret = Math.max(ret, tmp);
+            }
+        }
+
+        return ret;
+    }
 }
