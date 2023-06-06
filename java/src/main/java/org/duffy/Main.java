9f926f18a19c5bf0d4fdfe83ffe3b22c5b6c5ab7
zzunho9 <jamlove4763@naver.com>
2023-06-06 21:27:28 +0900

    feat: Solve no.2178 problem

--- java/src/main/java/org/duffy/Main.java
+++ java/src/main/java/org/duffy/Main.java
@@ -1,11 +1,20 @@
 package org.duffy;
 
 import java.io.*;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
+
+    public static int length, width;
+    public static int[][] grid, visited;
+    public static int[] dx = new int[] {0, -1, 0, 1};
+    public static int[] dy = new int[] {1, 0, -1, 0};
+
+    public static Queue<Integer[]> queue = new LinkedList<>();
+
+
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
     }
 }
