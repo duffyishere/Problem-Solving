6ab74d4aaac5a9b32456252e0df36c59418f5897
zzunho9 <jamlove4763@naver.com>
2023-06-16 01:07:07 +0900

    feat: Solve problem no.14501

--- java/src/main/java/org/duffy/bruteForce/Recursive.java
+++ java/src/main/java/org/duffy/bruteForce/Recursive.java
@@ -40,4 +40,33 @@ public class Recursive {
 
         return vowelCount >= 1 && consonantCount >= 2;
     }
+
+    int[] prices;
+    int[] days;
+    int n;
+    int ret = 0;
+
+    public void go14501(int day, int price) {
+        /*
+        1. False
+            if n < i:
+                return;
+        2. Answer
+            if day == n:
+                ret = max(ret, price);
+                return;
+        3. Next
+            go(day+days[day], price+prices[day]); >> 일을 하는 경우
+            go(day+1, price); >> 일을 하지 않는 경우
+         */
+
+        if (n+1 < day) return;
+        if (n+1 == day) {
+            ret = Math.max(ret, price);
+            return;
+        }
+
+        go14501(day+days[day], price+prices[day]);
+        go14501(day+1, price);
+    }
 }
