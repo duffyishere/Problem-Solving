798cc05cd5a78fe51ae30f57afb75b9a8b94c63e
zzunho9 <jamlove4763@naver.com>
2023-06-16 01:06:31 +0900

    feat: Solve problem no.14501

--- java/src/main/java/org/duffy/Main.java
+++ java/src/main/java/org/duffy/Main.java
@@ -8,5 +8,44 @@ public class Main {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        main.n = n;
+        main.prices = new int[n+1];
+        main.days = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            main.days[i] = Integer.parseInt(st.nextToken());
+            main.prices[i] = Integer.parseInt(st.nextToken());
+        }
+        main.go14501(1, 0);
+        System.out.println(main.ret);
+    }
+
+    int[] prices;
+    int[] days;
+    int n;
+    int ret = 0;
+
+    public void go14501(int day, int price) {
+        /*
+        1. False
+            if n < i:
+                return;
+        2. Answer
+            if day == n:
+                ret = max(ret, price);
+                return;
+        3. Next
+            go(day+days[day], price+prices[day]); >> 일을 하는 경우
+            go(day+1, price); >> 일을 하지 않는 경우
+         */
+
+        if (n+1 < day) return;
+        if (n+1 == day) {
+            ret = Math.max(ret, price);
+            return;
+        }
+
+        go14501(day+days[day], price+prices[day]);
+        go14501(day+1, price);
     }
 }
