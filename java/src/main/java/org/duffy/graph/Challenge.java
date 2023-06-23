cf139d9f4f0dff176e8441aae12e307aa1ff2baf
zzunho9 <jamlove4763@naver.com>
2023-06-23 15:32:00 +0900

    feat: Solve problem no.2146

--- java/src/main/java/org/duffy/graph/Challenge.java
+++ java/src/main/java/org/duffy/graph/Challenge.java
@@ -117,4 +117,86 @@ public class Challenge {
         for (int next: graph[now])
             dfs(next);
     }
+
+    int[] dy = {1, 0, -1, 0};
+    int[] dx = {0, -1, 0, 1};
+    public void pr2146(int n, int[][] graph) {
+        boolean[][] visited = new boolean[n][n];
+        int[][] groups = new int[n][n];
+
+        Queue<int[]> queue = new LinkedList<>();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j] && graph[i][j] == 1) {
+                    queue.add(new int[]{i, j});
+                    visited[i][j] = true;
+                    groups[i][j] = ++count;
+
+                    while (!queue.isEmpty()) {
+                        int[] tmp = queue.remove();
+                        int y = tmp[0];
+                        int x = tmp[1];
+                        for (int k = 0; k < 4; k++) {
+                            int my = y + dy[k];
+                            int mx = x + dx[k];
+                            if (mx < 0 || my < 0 || n <= mx || n <= my) continue;
+                            if (visited[my][mx]) continue;
+                            if (graph[my][mx] == 0) continue;
+
+                            queue.add(new int[]{my, mx});
+                            visited[my][mx] = true;
+                            groups[my][mx] = count;
+                        }
+                    }
+                }
+            }
+        }
+
+        queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                visited[i][j] = false;
+                if (groups[i][j] != 0) {
+                    queue.add(new int[]{i, j});
+                }
+            }
+        }
+
+        int[][] counts = new int[n][n];
+        while (!queue.isEmpty()) {
+            int[] tmp = queue.remove();
+            int y = tmp[0];
+            int x = tmp[1];
+
+            for (int i = 0; i < 4; i++) {
+                int my = y + dy[i];
+                int mx = x + dx[i];
+                if (mx < 0 || my < 0 || n <= mx || n <= my) continue;
+                if (graph[my][mx] != 0) continue;
+                if (visited[my][mx]) continue;
+
+                queue.add(new int[] {my, mx});
+                visited[my][mx] = true;
+                counts[my][mx] = counts[y][x] + 1;
+                groups[my][mx] = groups[y][x];
+            }
+        }
+
+        int ret = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < 4; k++) {
+                    int my = i + dy[k];
+                    int mx = j + dx[k];
+                    if (mx < 0 || my < 0 || n <= mx || n <= my) continue;
+                    if (groups[i][j] != groups[my][mx]) {
+                        ret = Math.min(ret, counts[i][j] + counts[my][mx]);
+                    }
+                }
+            }
+        }
+
+        System.out.println(ret);
+    }
 }
