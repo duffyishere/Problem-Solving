0637e9a18c9198029ead9308c6e52e7b1485ee1c
zzunho9 <jamlove4763@naver.com>
2023-07-07 00:47:06 +0900

    feat: Solve Leetcode problem no.209

---
+++ java/src/main/java/org/duffy/slidingWindow/SlidingWindow.java
@@ -0,0 +1,20 @@
+package org.duffy.slidingWindow;
+
+public class SlidingWindow {
+    public int minSubArrayLen(int target, int[] nums) {
+        int len = nums.length;
+        int start = 0;
+        int end = 0;
+        int minLen = len + 1;
+
+        while (end < len) {
+            target -= nums[end++];
+            while (target <= 0) {
+                minLen = Math.min(minLen, end - start);
+                target += nums[start++];
+            }
+        }
+
+        return minLen % (len + 1);
+    }
+}
