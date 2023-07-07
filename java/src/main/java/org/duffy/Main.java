72718a988fa146bd3d96df087cc8d33a8c70573b
zzunho9 <jamlove4763@naver.com>
2023-07-08 03:24:37 +0900

    feat: Solve the Leetcode 'Roman to Integer' problem

--- java/src/main/java/org/duffy/Main.java
+++ java/src/main/java/org/duffy/Main.java
@@ -1,6 +1,7 @@
 package org.duffy;
 
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String args[]) throws IOException {
--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -1,5 +1,8 @@
 package org.duffy.leet_code;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LeetCode {
     public int maxArea(int[] height) {
         int ret = 0;
@@ -15,6 +18,29 @@ public class LeetCode {
                 left++;
             else
                 right--;
+        }
+
+        return ret;
+    }
+
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int ret = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i + 1 < s.length() && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                ret -= map.get(s.charAt(i));
+            }
+            else {
+                ret += map.get(s.charAt(i));
+            }
         }
 
         return ret;
