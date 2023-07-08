54cb2680054606cda64b3dc99391eaf27a695acf
zzunho9 <jamlove4763@naver.com>
2023-07-08 13:53:25 +0900

    feat: Solve the Leetcode '3Sum' problem

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -1,8 +1,6 @@
 package org.duffy.leet_code;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class LeetCode {
     public int maxArea(int[] height) {
@@ -63,5 +61,34 @@ public class LeetCode {
         }
 
         return prefix;
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> ret = new ArrayList<>();
+
+        Arrays.sort(nums);
+        int target = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    set.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                }
+                else if (sum < target) {
+                    j++;
+                }
+                else {
+                    k--;
+                }
+            }
+        }
+
+        ret.addAll(set);
+        return ret;
     }
 }
