0d2beb8d4b516ce4456687b3c6a3c7989d610644
zzunho9 <jamlove4763@naver.com>
2023-07-09 15:41:11 +0900

    feat: Solve the Leetcode 'Remove Nth Node From End of List' problem

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -1,5 +1,7 @@
 package org.duffy.leet_code;
 
+import org.duffy.ListNode;
+
 import java.util.*;
 
 public class LeetCode {
@@ -115,5 +117,28 @@ public class LeetCode {
         }
 
         return ret;
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode start = new ListNode(0);
+        ListNode slow = start, fast = start;
+        slow.next = head;
+
+        for (int i = 0; i < n + 1; i++)
+            fast = fast.next;
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return start.next;
+    }
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 }
