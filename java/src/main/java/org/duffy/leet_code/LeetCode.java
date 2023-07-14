fa4b82814f55459a3b8c3241c47d766bab4a653c
zzunho9 <jamlove4763@naver.com>
2023-07-14 20:37:27 +0900

    feat: Solve the Leetcode 'Longest Valid Parentheses' problem

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -327,4 +327,26 @@ public class LeetCode {
 
         return nums;
     }
+
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int answer = 0;
+        stack.push(-1);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(')
+                stack.push(i);
+            else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                }
+                else {
+                    answer = Math.max(answer, i - stack.peek());
+                }
+            }
+        }
+
+        return answer;
+    }
 }
