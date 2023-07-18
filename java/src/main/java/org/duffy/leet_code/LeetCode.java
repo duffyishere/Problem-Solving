cdb4ce0aa3db2532afe3f5e7e75735dd259bf74b
zzunho9 <jamlove4763@naver.com>
2023-07-18 17:29:52 +0900

    feat: Solve the Leetcode 'First Missing Positive' problem

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -473,4 +473,28 @@ public class LeetCode {
             }
         }
     }
+
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        int len = nums.length;
+        while (i < len) {
+            int correct = nums[i] - 1;
+            if (nums[i] < len && 0 < nums[i] && nums[i] != nums[correct]) {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }
+            else {
+                i++;
+            }
+        }
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+                return j + 1;
+            }
+        }
+
+        return nums.length + 1;
+    }
 }
