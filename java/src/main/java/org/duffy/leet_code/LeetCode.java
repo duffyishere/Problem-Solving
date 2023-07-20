9e094beb6b75ee3483d13486414dd3dec4d1434e
zzunho9 <jamlove4763@naver.com>
2023-07-20 19:39:35 +0900

    refactor: Rename variables

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -70,20 +70,20 @@ public class LeetCode {
         Arrays.sort(nums);
         int target = 0;
         for (int i = 0; i < nums.length; i++) {
-            int j = i + 1;
-            int k = nums.length - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
                 if (sum == target) {
-                    set.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    k--;
+                    set.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
                 }
                 else if (sum < target) {
-                    j++;
+                    left++;
                 }
                 else {
-                    k--;
+                    right--;
                 }
             }
         }
