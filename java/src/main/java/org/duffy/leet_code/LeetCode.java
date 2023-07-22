e1ef86071022b93751bca864b4301524b1570abb
zzunho9 <jamlove4763@naver.com>
2023-07-22 16:49:26 +0900

    feat: Solve the Leetcode 'Maximum Subarray' problem

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -591,4 +591,17 @@ public class LeetCode {
 
         return new ArrayList<>(map.values());
     }
+
+    public int maxSubArray(int[] nums) {
+        int len = nums.length;
+        int[] dp = new int[len + 1];
+        dp[0] = nums[0];
+        int ans = nums[0];
+        for (int i = 1; i < len; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            ans = Math.max(ans, dp[i]);
+        }
+
+        return ans;
+    }
 }
