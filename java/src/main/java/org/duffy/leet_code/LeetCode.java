5ef19bf9e08f445fbe822a9c6a17e46ad54823d9
zzunho9 <jamlove4763@naver.com>
2023-07-24 16:51:22 +0900

    feat: Solve the Leetcode 'Jump Game' problem

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -604,4 +604,33 @@ public class LeetCode {
 
         return ans;
     }
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int left = 0, right = matrix[0].length - 1;
+        int top = 0, bottom = matrix.length - 1;
+        List<Integer> ans = new ArrayList<>();
+
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++)
+                ans.add(matrix[top][i]);
+            top++;
+
+            for (int i = top; i <= bottom; i++)
+                ans.add(matrix[i][right]);
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; left <= i; i--)
+                    ans.add(matrix[bottom][i]);
+            }
+            bottom--;
+
+            if (left <= right) {
+                for (int i = bottom; top <= i; i--)
+                    ans.add(matrix[i][left]);
+            }
+            left++;
+        }
+        return ans;
+    }
 }
