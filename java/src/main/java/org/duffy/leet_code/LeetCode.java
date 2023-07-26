e9c3b8ae0ec70292ca3fc818a25cd29f69bae9e4
zzunho9 <jamlove4763@naver.com>
2023-07-26 16:37:45 +0900

    feat: Solve the Leetcode 'Climbing Stairs' problem

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -688,4 +688,18 @@ public class LeetCode {
         }
         return grid[m - 1][n - 1];
     }
+
+    public int climbStairs(int n) {
+        if (n == 1)
+            return 1;
+
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        return dp[n - 1];
+    }
 }
