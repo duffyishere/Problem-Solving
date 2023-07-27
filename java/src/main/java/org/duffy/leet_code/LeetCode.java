fe3807aa8d99741db6d686d4a07bcd9c7f4a3ecb
zzunho9 <jamlove4763@naver.com>
2023-07-27 17:23:46 +0900

    feat: Solve the Leetcode 'Set Matrix Zeroes' problem

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -722,4 +722,42 @@ public class LeetCode {
         }
         return dp[m][n];
     }
+
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean rowFlag = false, colFlag = false;
+
+        for (int i = 0; i < m; i++)
+            if (matrix[i][0] == 0)
+                rowFlag = true;
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0)
+                colFlag = true;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+        }
+
+        if (rowFlag) {
+            for (int i = 0; i < m; i++)
+                matrix[i][0] = 0;
+        }
+        if (colFlag) {
+            for (int i = 0; i < n; i++)
+                matrix[0][i] = 0;
+        }
+    }
 }
