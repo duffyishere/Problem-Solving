8a572391318324c4147b85ca317debb563379fea
zzunho9 <jamlove4763@naver.com>
2023-07-28 23:12:57 +0900

    feat: Solve the Leetcode 'Sort Colors' problem

--- java/src/main/java/org/duffy/Main.java
+++ java/src/main/java/org/duffy/Main.java
@@ -1,10 +1,43 @@
 package org.duffy;
 
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String args[]) throws IOException {
         Main main = new Main();
+    }
+
+    private void mergeSort(int[] nums, int left, int right) {
+        int mid = (left + right) / 2;
+        if (left + 1 == right)
+            return;
+
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid, right);
+        merge(nums, left, mid, right);
+    }
+
+    private void merge(int[] nums, int left, int mid, int right) {
+        int[] tmp = new int[right - left + 1];
+        int leftIndex = left;
+        int rightIndex = mid;
+
+        for (int i = left; i < right; i++) {
+            if (rightIndex == right) {
+                tmp[i] = nums[leftIndex++];
+            }
+            else if (leftIndex == mid) {
+                tmp[i] = nums[rightIndex++];
+            }
+            else if (nums[leftIndex] < nums[rightIndex]) {
+                tmp[i] = nums[leftIndex++];
+            }
+            else {
+                tmp[i] = nums[rightIndex++];
+            }
+        }
+
+        for (int i = left; i < right; i++)
+            nums[i] = tmp[i];
     }
 }
