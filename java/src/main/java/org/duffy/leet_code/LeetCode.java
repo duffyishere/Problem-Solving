b4abd7580b37d8b6aab48e8d339a746942e93b2e
zzunho9 <jamlove4763@naver.com>
2023-08-29 20:46:46 +0900

    feat: Solve the Leetcode '136. Single Number problem'

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -1124,4 +1124,13 @@ public class LeetCode {
         }
         return false;
     }
+
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i += 2) {
+            if (nums[i] != nums[i + 1])
+                return nums[i - 1];
+        }
+        return nums[nums.length - 1];
+    }
 }
