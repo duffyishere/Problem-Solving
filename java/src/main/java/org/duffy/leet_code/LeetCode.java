4871671e069d6417fd0d0b69a34346b1ecf77d6d
zzunho9 <jamlove4763@naver.com>
2023-09-02 19:08:06 +0900

    feat: Solve the Leetcode '100. Same Tree'

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -1133,4 +1133,18 @@ public class LeetCode {
         }
         return nums[nums.length - 1];
     }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return isSameTreeHelper(p, q);
+    }
+    private boolean isSameTreeHelper(TreeNode n1, TreeNode n2) {
+        if (n1 == null && n2 != null || n1 != null && n2== null)
+            return false;
+        if (n1 == null && n2 == null)
+            return true;
+        if (n1.val != n2.val)
+            return false;
+
+        return isSameTreeHelper(n1.left, n2.left) && isSameTreeHelper(n1.right, n2.right);
+    }
 }
