64a3dc541a8e2dd65c4aaec73530c467eabf5487
zzunho9 <jamlove4763@naver.com>
2023-09-10 18:53:26 +0900

    feat: Solve the Leetcode '141. Linked List Cycle' problem

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -1206,4 +1206,18 @@ public class LeetCode {
         dp[idx] = 0;
         return false;
     }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode runner = head;
+        ListNode walker = head;
+
+        while (runner.next != null && runner.next.next != null) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if (walker == runner)
+                return true;
+        }
+        return false;
+    }
 }
