206f70eb930fee8ebb9e36275f9a9157aa4f6fb2
zzunho9 <jamlove4763@naver.com>
2023-09-24 18:53:30 +0900

    feat: Solve the Leetcode '160. Intersection of Two Linked Lists' problem

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -1311,4 +1311,15 @@ public class LeetCode {
             }
         }
         return nums[left];
-    }}
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            a = a == null? headB: a.next;
+            b = b == null? headA: b.next;
+        }
+        return a;
+    }
+}
