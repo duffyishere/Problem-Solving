9fe0e5f57385811f23363135ae40c1195a9fdf57
zzunho9 <jamlove4763@naver.com>
2023-09-24 18:26:45 +0900

    feat: Solve the Leetcode '153. Find Minimum in Rotated Sorted Array' problem

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -1297,4 +1297,18 @@ public class LeetCode {
         }
         return ret;
     }
-}
+
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[right] < nums[mid]) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }}
