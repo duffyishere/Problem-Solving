747f9fd24b4c5c89e83adfbc46779bfab6bf8349
zzunho9 <jamlove4763@naver.com>
2023-09-25 20:29:46 +0900

    feat: Solve the Leetcode '206. Reverse Linked List' problem

--- java/src/main/java/org/duffy/Main.java
+++ java/src/main/java/org/duffy/Main.java
@@ -1,6 +1,9 @@
 package org.duffy;
 
+import org.duffy.leet_code.ListNode;
+
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String args[]) {
--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -1414,4 +1414,15 @@ public class LeetCode {
             numIslandsDFS(my, mx);
         }
     }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
 }
