6ccf466b679db71458aa2340058d11a2e65819c1
zzunho9 <jamlove4763@naver.com>
2023-10-24 20:20:41 +0900

    feat: Add '322. Coin Change'

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -1608,4 +1608,17 @@ public class LeetCode {
         }
         return max;
     }
+
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+
+        for (int coin: coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        return dp[amount] < amount + 1? dp[amount]: -1;
+    }
 }
