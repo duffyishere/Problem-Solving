896581e86fc672d903e533f7385e45544ec45121
zzunho9 <jamlove4763@naver.com>
2023-11-11 16:47:31 +0900

    Feat: 프로그래머스 '가장 큰 수' 문제 풀이 추가

--- java/src/main/java/org/duffy/programmers/Solution.java
+++ java/src/main/java/org/duffy/programmers/Solution.java
@@ -1,8 +1,6 @@
 package org.duffy.programmers;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
 
@@ -42,5 +40,24 @@ public class Solution {
                 return false;
         }
         return true;
+    }
+
+    public String 가장_큰_수(int[] numbers) {
+        List<String> str = new ArrayList<>();
+        for (int num: numbers) str.add(String.valueOf(num));
+
+        str.sort(((o1, o2) -> {
+            String s1 = o1 + o2;
+            String s2 = o2 + o1;
+            return s2.compareTo(s1);
+        }));
+
+        StringBuilder sb = new StringBuilder();
+        for (String s: str)
+            sb.append(s);
+
+        if (str.get(0).charAt(0) == '0') return "0";
+
+        return sb.toString();
     }
 }
