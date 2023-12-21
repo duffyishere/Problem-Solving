5548d621d5640da5afc99003e1e315b60ce4e3e4
zzunho9 <jamlove4763@naver.com>
2023-12-21 23:15:01 +0900

    Feat: LC '1637. Widest Vertical Area Between Two Points Containing No Points' 문제 풀이 추가

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -2461,4 +2461,13 @@ public class LeetCode {
         }
         return res;
     }
+
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, Comparator.comparingInt(a -> a[0]));
+        int res = 0;
+        for (int i = 1; i < points.length; i++) {
+            res = Math.max(res, points[i][0] - points[i - 1][0]);
+        }
+        return res;
+    }
 }
