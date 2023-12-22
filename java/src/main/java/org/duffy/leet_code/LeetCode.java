e16fe200b18da1672678d46e97e5f4a8734f2699
zzunho9 <jamlove4763@naver.com>
2023-12-23 03:41:12 +0900

    Feat: LC '287. Find the Duplicate Number' 문제 풀이 추가

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -2492,4 +2492,19 @@ public class LeetCode {
         }
         return res;
     }
+
+    public int findDuplicate2(int[] nums) {
+        int slow = nums[0], fast = nums[nums[0]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
 }
