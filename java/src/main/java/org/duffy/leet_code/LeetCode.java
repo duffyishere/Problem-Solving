f57c7a60a5e7834d1c71eac1c78df49c348f6b3a
zzunho9 <jamlove4763@naver.com>
2023-12-31 18:20:19 +0900

    Feat: LC '1624. Largest Substring Between Two Equal Characters' 문제 풀이 추가

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -2772,4 +2772,18 @@ public class LeetCode {
         }
         return res;
     }
+
+    public int maxLengthBetweenEqualCharacters(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int res = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                res = Math.max(res, i - map.get(c) - 1);
+            } else {
+                map.put(c, i);
+            }
+        }
+        return res;
+    }
 }
