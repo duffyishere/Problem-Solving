21a0b248870812b9bc096eabc83878c67c171b13
zzunho9 <jamlove4763@naver.com>
2024-01-08 17:05:24 +0900

    Feat: LC '704. Binary Search' 문제 풀이 추가

--- java/src/main/java/org/duffy/leet_code/LeetCode.java
+++ java/src/main/java/org/duffy/leet_code/LeetCode.java
@@ -2899,4 +2899,18 @@ public class LeetCode {
         }
         return true;
     }
+
+    public int search_2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left] == target? left: -1;
+    }
 }
