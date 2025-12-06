b0e1c9e40ad7d99dd293ad10e705917bdeaa3a50
zzunho9 <jamlove4763@naver.com>
2025-12-06 13:58:12 +0900

    feat: 트럭 주차(no.2979) 문제 풀이 추가

---
+++ python/pr2979.py
@@ -0,0 +1,24 @@
+import sys
+
+
+input = sys.stdin.readline
+a, b, c = map(int, input().split())
+b = b * 2
+c = c * 3
+array: int = [0 for _ in range(101)]
+
+for _ in range(3):
+    start, end = map(int, input().split())
+    for i in range(start, end):
+        if array[i] == 0:
+            array[i] = a
+        elif array[i] == a:
+            array[i] = b
+        elif array[i] == b:
+            array[i] = c
+
+result: int = 0
+for price in array:
+    if price != 0:
+        result += price
+print(result)
