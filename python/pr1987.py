513c1bf65639918fbda9ba3e67b330626ca56828
zzunho9 <jamlove4763@naver.com>
2025-12-20 21:54:47 +0900

    feat: 알파벳(no.1987) 문제풀이 추가

---
+++ python/pr1987.py
@@ -0,0 +1,35 @@
+from collections import deque
+import sys
+
+
+def main():
+    input = sys.stdin.readline
+    n, m = map(int, input().split())
+    graph = [input().rstrip() for _ in range(n)]
+
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    result = 1
+
+    def dfs(y, x, cnt):
+        nonlocal visited, result
+        result = max(result, cnt)
+        for dy, dx in directions:
+            my = y + dy
+            mx = x + dx
+            if my < 0 or mx < 0 or n <= my or m <= mx:
+                continue
+
+            next_word = ord(graph[my][mx]) - 65
+            if visited[next_word]:
+                continue
+
+            visited[next_word] = True
+            dfs(my, mx, cnt + 1)
+            visited[next_word] = False
+
+    visited = [False] * 26
+    visited[ord(graph[0][0]) - 65] = True
+    dfs(0, 0, 1)
+    print(result)
+if __name__ == "__main__":
+    main()
