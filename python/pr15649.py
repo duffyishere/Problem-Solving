51cada4e75c84f6e9ff98e8eb9f21ab690fdac42
zzunho9 <jamlove4763@naver.com>
2026-02-09 12:02:27 +0900

    feat: N과 M 1(no.15649) 문제풀이 추가

---
+++ python/pr15649.py
@@ -0,0 +1,17 @@
+import sys
+
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+def combi(arr: list):
+    if len(arr) == m:
+        print(*arr)
+        return
+    
+    for i in range(1, n + 1):
+        if i in arr: continue
+
+        combi(arr + [i])
+
+combi([])
