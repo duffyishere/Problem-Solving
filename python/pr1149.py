b553b8745d2a855e7efcae64767628b2d6c7920d
zzunho9 <jamlove4763@naver.com>
2026-02-14 11:29:25 +0900

    solve: RGB거리(no.1149) 문제풀이 추가

---
+++ python/pr1149.py
@@ -0,0 +1,16 @@
+import sys
+
+
+input = sys.stdin.readline
+n = int(input())
+prices = [list(map(int, input().split())) for _ in range(n)]
+d = [list([1000010] * 3) for _ in range(n)]
+d[0] = prices[0]
+
+# d[idx][0] = min(d[idx - 1][1], d[idx - 1][2]) + price[idx][0]
+for i in range(1, n):
+    d[i][0] = min(d[i - 1][1], d[i - 1][2]) + prices[i][0]
+    d[i][1] = min(d[i - 1][0], d[i - 1][2]) + prices[i][1]
+    d[i][2] = min(d[i - 1][1], d[i - 1][0]) + prices[i][2]
+
+print(min(d[n - 1]))
