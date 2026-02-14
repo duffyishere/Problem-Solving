f2b4d290c03f1a61310d2c7635cdbe8eb28cad1b
zzunho9 <jamlove4763@naver.com>
2026-02-14 12:27:20 +0900

    solve: 2xn 타일링(no.11726) 문제풀이 추가

---
+++ python/pr11726.py
@@ -0,0 +1,14 @@
+import sys
+
+
+input = sys.stdin.readline
+n = int(input().rstrip())
+mod = 10007
+d = [0] * 10005
+d[1] = 1
+d[2] = 2
+
+for i in range(3, n + 1):
+    d[i] = d[i - 1] + d[i - 2]
+    d[i] %= mod
+print(d[n])
